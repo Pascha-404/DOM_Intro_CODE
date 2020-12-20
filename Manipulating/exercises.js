17f6c8616a5d6eb52ad30259889d82061e7ef72a
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-20 22:40:01 +0100

    loooooooop

--- Manipulating/exercises.js
+++ Manipulating/exercises.js
@@ -1,3 +1,14 @@
 const firstImage = document.querySelector('#banner');
 
 // firstImage.src = 'https://images.unsplash.com/photo-1501200291289-c5a76c232e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2781&q=80'
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+
+const letters = document.querySelectorAll('span');
+
+for (let i = 0; i < colors.length; i++){
+
+    letters[i].style.color = colors[i];
+}
