c1d2e663cbb9cfdaeca6fdee5c2a8c20714dad21
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-20 21:27:57 +0100

    trained 1

--- Selecting/exercises.js
+++ Selecting/exercises.js
@@ -2,4 +2,4 @@ const image = document.getElementById('banner');
 const heading = document.getElementById('heading')
 
 const toc = document.querySelectorAll('.toclevel-1')
-const list = document.querySelector('ul:last-of-typet')
+const list = document.querySelector('ul:last-of-type')
