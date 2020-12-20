ecff80ddd541809d0b5401dc96395940adf945b8
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-20 19:51:15 +0100

    worked with query selector

--- Selecting/exercises.js
+++ Selecting/exercises.js
@@ -1,2 +1,5 @@
 const image = document.getElementById('banner');
 const heading = document.getElementById('heading')
+
+const toc = document.querySelectorAll('.toclevel-1')
+const list = document.querySelector('ul:last-of-typet')
