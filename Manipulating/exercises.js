66af55023499120eff64ce3c087ecd6d6303f2e5
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-20 21:43:45 +0100

    trained #3

---
+++ Manipulating/exercises.js
@@ -0,0 +1,3 @@
+const firstImage = document.querySelector('#banner');
+
+// firstImage.src = 'https://images.unsplash.com/photo-1501200291289-c5a76c232e5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2781&q=80'
